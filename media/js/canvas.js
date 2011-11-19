02a82a10b0ba1b934ded1454917cf3fe45cd70cd
George Tebbetts <george.tebbetts@gmail.com>
2011-11-19 20:29:17 +0000

    Draws red cirlce(future joystick)

--- media/js/canvas.js
+++ media/js/canvas.js
@@ -1,55 +1,46 @@
-$(window).addEvent('load',function(){
+var WIDTH = 500;
+var HEIGHT = 500;
+
+var c = document.createElement('canvas');
+c.setAttribute('id', 'canvas');
+c.setAttribute('width', WIDTH);
+c.setAttribute('height', HEIGHT);
+
+document.getElementsByTagName('body')[0].appendChild(c)
+
+$(window).addEvent('load', function(){
 
 	//initialize the CANVAS object
 	CANVAS.init({ canvasElement : 'canvas' });
 	
 	//attach a new layer
-	CANVAS.layers.add( new Layer({
+	var layer = CANVAS.layers.add( new Layer({
 		id : 'myLayer'
 	}));
 	
-	//set globalCompositeOperation on the canvas
-	CANVAS.ctx.globalCompositeOperation = 'lighter';
-
-	//just some arrays
-	var colors = [
-		'rgba(255,0,0,1)',
-		'rgba(0,255,0,1)',
-		'rgba(0,0,255,1)'
-		];
-	
-	var pos = [
-		{ x: 150, y : 100 },
-		{ x: 200, y : 150 },
-		{ x: 250, y : 50 }
-	]
-	
-	//create three items on the layer
-	for(var i = 0; i < colors.length; i++)
-	{
-		/* select the layer, then add a new CanvasItem
-		 * with the property from the respective array
-		 * */
-		
-		CANVAS.layers.get('myLayer').add( new CanvasItem({
-			id : 'item-'+i,
-			x : pos[i].x,
-			y : pos[i].y,
-			fillStyle : colors[i],
-			events : {
-				onDraw : function(ctx)
-				{
-					/* this is the code that is called
-					 * every time the canvas is drawn.
-					 * */
-					ctx.fillStyle = this.fillStyle;
-					ctx.fillRect(this.x,this.y,200,200);
-				}
+	layer.add({
+		id : 'joystick',
+		x : WIDTH / 2,
+		y : HEIGHT / 2,
+		radius : 30,
+		events : {
+			onDraw : function(ctx) {
+				ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+				ctx.lineWidth = 8;
+				ctx.strokeStyle = 'rgba(0,0,0,1)';
+				ctx.stroke();
+				ctx.fillStyle = 'rgba(255,0,0,1)';
+				ctx.fill();
 			}
-		}));
-	}
-	
-	//finally draw the canvas once
-	CANVAS.draw();
+		}
+	});
+
+	CANVAS.addThread(new Thread({
+		id : 'myThread',
+		onExec : function(){
+			CANVAS.clear().draw();
+		}
+	}));
+
 });
 
